fd6b10a854db457340630c96552b85024d43474a
Alin Stanciu <alinstanciu2011@yahoo.com>
2024-01-16 16:33:09 +0200

    thd: add domain

---
+++ NBodyThreadedSimulation/NBodyThreadedSimulation/Domain/Body.cs
@@ -0,0 +1,15 @@
+﻿namespace NBodyThreadedSimulation.Domain;
+
+record Body
+{
+    public double Mass { get; set; }
+    public Vector3D? Position { get; set; }
+    public Vector3D? Velocity { get; set; }
+
+    public Body DeepCopy() => new()
+    {
+        Mass = Mass,
+        Position = (Position ?? new()) with { },
+        Velocity = (Velocity ?? new()) with { },
+    };
+}
---
+++ NBodyThreadedSimulation/NBodyThreadedSimulation/Domain/Scene.cs
@@ -0,0 +1,13 @@
+﻿namespace NBodyThreadedSimulation.Domain;
+
+record Scene
+{
+    public double GravitationalConstant { get; set; }
+    public List<Body>? Bodies { get; set; }
+
+    public Scene DeepCopy() => new()
+    {
+        GravitationalConstant = GravitationalConstant,
+        Bodies = (Bodies ?? new()).Select(b => b.DeepCopy()).ToList(),
+    };
+}
